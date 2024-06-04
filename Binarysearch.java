d1ef76404344e388f325f10d86b1fd0b5df11f44
Kelvin Raffael <166054807+Kwenzen2930@users.noreply.github.com>
2024-06-04 15:31:39 -0700

    Update Binarysearch.java

--- Binarysearch.java
+++ Binarysearch.java
@@ -1,9 +1,9 @@
 class Node {
-
     int data;
     Node left;
     Node right;
 
+    // Constructor to create a new node with the given value
     Node(int k) {
         this.data = k;
         this.left = this.right = null;
@@ -13,11 +13,12 @@ class Node {
 class Binarysearch {
     Node root;
 
-    // Inorder
+    // Method to start the inorder traversal from the root
     public void printInorder() {
         printInorder(root);
     }
 
+    // Inorder traversal: left, root, right
     private void printInorder(Node node) {
         if (node == null)
             return;
@@ -26,11 +27,12 @@ class Binarysearch {
         printInorder(node.right);
     }
 
-    // Preorder Traversal
+    // Method to start the preorder traversal from the root
     public void printPreorder() {
         printPreorder(root);
     }
 
+    // Preorder traversal: root, left, right
     private void printPreorder(Node node) {
         if (node == null)
             return;
@@ -39,11 +41,12 @@ class Binarysearch {
         printPreorder(node.right);
     }
 
-    // Postorder Traversal
+    // Method to start the postorder traversal from the root
     public void printPostorder() {
         printPostorder(root);
     }
 
+    // Postorder traversal: left, right, root
     private void printPostorder(Node node) {
         if (node == null)
             return;
@@ -52,15 +55,19 @@ class Binarysearch {
         System.out.print(node.data + " ");
     }
 
+    // Method to add a new value to the tree
     public void add(int value) {
         root = add(root, value);
     }
 
+    // Recursive method to add a value to the tree
     private Node add(Node node, int value) {
+        // If the tree/subtree is empty, create a new node
         if (node == null) {
             return new Node(value);
         }
 
+        // Recursively add to the left or right subtree
         if (value < node.data) {
             node.left = add(node.left, value);
         } else if (value > node.data) {
@@ -70,54 +77,72 @@ class Binarysearch {
         return node;
     }
 
+    // Method to remove a value from the tree
     public void remove(int value) {
         root = remove(root, value);
     }
 
+    // Recursive method to remove a value from the tree
     private Node remove(Node node, int value) {
+        // Base case: if the tree is empty
         if (node == null) {
             return node;
         }
 
+        // Recursively find the node to remove
         if (value < node.data) {
             node.left = remove(node.left, value);
         } else if (value > node.data) {
             node.right = remove(node.right, value);
         } else {
+            // Node to be removed found
+
+            // Case 1: Node with only one child or no child
             if (node.left == null) {
                 return node.right;
             } else if (node.right == null) {
                 return node.left;
             }
 
+            // Case 2: Node with two children
+            // Get the inorder successor (smallest in the right subtree)
             node.data = findMinValue(node.right);
+
+            // Delete the inorder successor
             node.right = remove(node.right, node.data);
         }
 
         return node;
     }
 
+    // Method to search for a value in the tree
     public boolean search(int value) {
         return search(root, value);
     }
 
+    // Recursive method to search for a value
     private boolean search(Node node, int value) {
+        // Base case: if the tree is empty
         if (node == null) {
             return false;
         }
 
+        // Recursively search in the left or right subtree
         if (value < node.data) {
             return search(node.left, value);
         } else if (value > node.data) {
             return search(node.right, value);
         } else {
+            // Value found
             return true;
         }
     }
 
+    // Method to find the minimum value in a subtree
     private int findMinValue(Node node) {
         int minValue = node.data;
 
+        // Loop to find the leftmost leaf
         while (node.left != null) {
             minValue = node.left.data;
             node = node.left;
